d4f2bf65797f2c8abef9560f747f102db608aa85
Sayurami <sayuramihiranga4@gmail.com>
2025-07-03 11:51:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const bodyParser = require("body-parser");
+const bodyParser = require('body-parser');
+
+const __path = process.cwd();
 const PORT = process.env.PORT || 8000;
 
 const server = require('./qr');
@@ -9,31 +11,26 @@ const code = require('./pair');
 
 require('events').EventEmitter.defaultMaxListeners = 500;
 
-// Body parser මුලින්ම
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// API Routes
 app.use('/server', server);
 app.use('/code', code);
 
-// HTML Routes
 app.get('/pair', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'pair.html'));
+  res.sendFile(path.join(__path, 'pair.html'));
 });
 
 app.get('/qr', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'qr.html'));
+  res.sendFile(path.join(__path, 'qr.html'));
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(process.cwd(), 'main.html'));
+  res.sendFile(path.join(__path, 'main.html'));
 });
 
-// Server Start
 app.listen(PORT, () => {
-    console.log(`⭐ Don't forget to give a star!`);
-    console.log(`🚀 Server running on http://localhost:${PORT}`);
+  console.log(`Server running on http://localhost:${PORT}`);
 });
 
 module.exports = app;
