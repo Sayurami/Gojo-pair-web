feba2ae5d1b24759f930d18b1471c65085527d54
Sayurami <sayuramihiranga4@gmail.com>
2025-09-17 22:43:10 +0530

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,20 +1,14 @@
-const jwt = require("jsonwebtoken");
+const ADMIN_USERNAME = "sayuramihiranga4@gmail.com";
+const ADMIN_PASSWORD = "Sayura2008***7";
 
-const SECRET = process.env.JWT_SECRET || "Sayura2008***7111s";
-
-function generateToken(user) {
-  return jwt.sign({ username: user.username }, SECRET, { expiresIn: "1h" });
-}
-
-function verifyToken(req, res, next) {
-  const token = req.headers["authorization"];
-  if (!token) return res.status(403).json({ error: "No token provided" });
-
-  jwt.verify(token.replace("Bearer ", ""), SECRET, (err, decoded) => {
-    if (err) return res.status(401).json({ error: "Unauthorized" });
-    req.user = decoded;
+// Simple verify middleware
+function verifyAdmin(req, res, next){
+  const { username, password } = req.body; // browser form POST username/password
+  if(username === ADMIN_USERNAME && password === ADMIN_PASSWORD){
     next();
-  });
+  } else {
+    res.status(403).json({ success: false, message: "Unauthorized" });
+  }
 }
 
-module.exports = { generateToken, verifyToken };
+module.exports = { verifyAdmin };
