a8862c7e586c5f61e947692f8ce121959c7e591a
Sayurami <sayuramihiranga4@gmail.com>
2025-09-17 22:13:32 +0530

    Create auth.js

---
+++ auth.js
@@ -0,0 +1,20 @@
+const jwt = require("jsonwebtoken");
+
+const SECRET = process.env.JWT_SECRET || "supersecretkey";
+
+function generateToken(user) {
+  return jwt.sign({ username: user.username }, SECRET, { expiresIn: "1h" });
+}
+
+function verifyToken(req, res, next) {
+  const token = req.headers["authorization"];
+  if (!token) return res.status(403).json({ error: "No token provided" });
+
+  jwt.verify(token.replace("Bearer ", ""), SECRET, (err, decoded) => {
+    if (err) return res.status(401).json({ error: "Unauthorized" });
+    req.user = decoded;
+    next();
+  });
+}
+
+module.exports = { generateToken, verifyToken };
