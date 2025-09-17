7a1a5f256054b1069d44a0af7d17167089fdce7b
Sayurami <sayuramihiranga4@gmail.com>
2025-09-17 16:04:25 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,5 @@
+module.exports = {
+  PORT: process.env.PORT || 3000,
+  MEGA_EMAIL: process.env.MEGA_EMAIL || 'nnarutouzumaki25000@gmail.com',
+  MEGA_PASSWORD: process.env.MEGA_PASSWORD || 'Sayura2008***7'
+};
