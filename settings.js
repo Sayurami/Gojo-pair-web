67d5596b9c3a49981e7a2b6707b666093d6e763e
Sayurami <sayuramihiranga4@gmail.com>
2025-09-18 11:05:46 +0530

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,8 @@
+module.exports = {
+  PORT: 3000,
+  JWT_SECRET: 'Sayura2008***7111s',
+  ADMIN_USERNAME: 'sayura',
+  ADMIN_PASSWORD: 'Sayura2008***7',
+  MEGA_EMAIL: 'nnarutouzumaki25000@gmail.com',
+  MEGA_PASSWORD: 'Sayura2008***8'
+};
